50924470116482ef5b1c817075db6b2d16b14a6c
L0RR4 <arlensonlorran@gmail.com>
2024-09-27 17:45:56 -0300

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,38 @@
+#########################
+#
+# Nome: Arlenson Lorran
+#
+# Data: 27/09/2024
+#
+#########################
+
+# FUNÇÕES
+
+
+
+# IMPORTAR BIBLIOTECAS
+
+
+
+# OPERANDOS
+
+mamiferos_d = {
+'Mamiferos': 'morcego', 'gato', 'vaca', 'hipopotamo'}
+aves_d = {
+'Aves': 'galinha', 'pinguim', 'flamingo', 'pelicano'}
+repteis_d = {
+'Reptéis': 'camaleão', 'lagartixa', 'cobra', 'tartaruga'}    
+peixes_d = {
+'Peixes': 'tilapia', 'sardinha', 'tubarão', 'enguia'}
+anfibios_d{'Anfibios': 'sapo', 'axolot', 'rãs', 'perereca'}    
+
+# ENTRADA
+
+
+
+# PROCESSAMENTO
+
+
+
+# SAIDA
+
